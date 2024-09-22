1f6ba02795e2d53bc5cedebd54e472fd1b8ae760
sulakhan-singh <singhsulakhan72767@gmail.com>
2024-09-22 15:54:59 +0000

    coversion

---
+++ basics/conversion.js
@@ -0,0 +1,21 @@
+let num=33;
+console.log(typeof num);
+let InString=String(num);
+console.log(typeof InString);
+let bv=false;
+console.log(typeof bv);
+let innum =Number(bv);
+
+console.log(innum);
+let str=""
+console.log(Number(str));
+let strrr="sukh";
+console.log(Number(strrr));
+console.log(Boolean(strrr));
+
+
+
+
+
+
+
