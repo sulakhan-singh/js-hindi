1b3fe25d1b9a6706342bcff5666ebd9aebac048c
sulakhan-singh <singhsulakhan72767@gmail.com>
2024-10-13 15:45:59 +0000

    stringMathods

---
+++ basics/string2.js
@@ -0,0 +1,24 @@
+const roll=392
+const name="sulakhan"
+console.log(`hello my name is ${name} and my rollno is ${roll}`)
+const game=new String("sulakhan singh")
+console.log(game[0])
+console.log(game.length);
+console.log(game.toUpperCase());
+console.log(game.charAt(3));
+console.log(game.indexOf("s"));
+const url="my name is sukh"
+console.log(url.includes("sukh"));
+console.log(url.split(' '));
+console.log(url);
+console.log(url.trim());
+console.log(name.substring(1,3));
+console.log(url.slice(4,8));
+
+
+
+
+
+
+
+
