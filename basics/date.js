cb7a97719da1fd625a12790c9ab0310c9bbabca8
sulakhan-singh <singhsulakhan72767@gmail.com>
2024-11-03 15:56:12 +0000

    dates

---
+++ basics/date.js
@@ -0,0 +1,14 @@
+//let myDate=new Date(2023,1,21)
+//console.log(myDate);
+//console.log(myDate.toDateString());
+//console.log();
+//let timestamp=Math.floor(Date.now()/1000)
+//console.log(timestamp);
+//console.log(myDate.getTime());
+let newdat=new Date()
+console.log(newdat.getMonth());
+console.log(newdat.getFullYear());
+
+
+
+
