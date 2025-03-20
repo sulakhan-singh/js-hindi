a18d6dfbe58d2ba51eef9e333bb79ecac62b5a43
sulakhan-singh <singhsulakhan72767@gmail.com>
2025-03-20 15:16:27 +0000

    objects

---
+++ basics/object.js
@@ -0,0 +1,17 @@
+//object litrals
+const jsuser= {
+    name:"sukh",
+    age:18,
+    email:"singhsulakhan72767@gmail.com"
+    lastdays:["saturday","sunday"],
+}
+//fisrt method to access objects
+console.log(jsuser.name);
+console.log(jsuser.email);
+console.log(jsuser.lastdays);
+
+//other method 
+console.log(jsuser[name]);
+
+
+
